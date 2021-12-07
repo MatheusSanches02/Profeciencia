702aa3a3be69e4c3b4b4cfae113282989e56e85f
MatheusSanches02 <rm87426@fiap.com.br>
2021-12-07 14:48:09 -0300

    Projeto finalizado em partes

---
+++ CadastroClientes/CadastroClientes/Data/DotNetCoreMySQLContext.cs
@@ -0,0 +1,15 @@
+﻿using CadastroClientes.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroClientes.Data
+{
+    public class DotNetCoreMySQLContext : DbContext
+    {
+        public DotNetCoreMySQLContext(DbContextOptions<DotNetCoreMySQLContext> options) : base(options) { }
+        public DbSet<Pessoa> PessoaFisica { get; set; }
+    }
+}
---
+++ CadastroClientes/CadastroClientes/Models/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroClientes.Models
+{
+    public class Pessoa
+    {
+        public long Cpf { get; set; }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public int Telefone { get; set; }
+
+        public string Endereco { get; set; }
+    }
+}
